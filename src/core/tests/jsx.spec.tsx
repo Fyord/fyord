b2c3e8a1c44781f6bdcca2460a06a0ab3c46687d
Joey Bayes <joey.bayes@uscca.com>
2022-02-26 12:27:43 -0500

    Fix tests (add await).

--- src/core/tests/jsx.spec.tsx
+++ src/core/tests/jsx.spec.tsx
@@ -8,16 +8,16 @@ import { TestHelpers } from '../../utilities/testHelpers';
 import { Strings } from 'tsbase/System/Strings';
 
 describe('JsxRenderer', () => {
-  it('should return the outer html of a parsed jsx node with only inner text', () => {
+  it('should return the outer html of a parsed jsx node with only inner text', async () => {
     const jsxToParse = <div>test</div>;
     const expectedOuterHtml = '<div>test</div>';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
   });
 
-  it('should return the outer html of a parsed jsx node with a nested node', () => {
+  it('should return the outer html of a parsed jsx node with a nested node', async () => {
     const jsxToParse = <div><p>test</p></div>;
     const expectedOuterHtml = '<div><p>test</p></div>';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
   });
 
   it('should return the outer html of a parsed jsx node with a nested component', async () => {
@@ -25,32 +25,32 @@ describe('JsxRenderer', () => {
       Template = async () => <>test</>;
     }
     const jsxToParse = <div>{await new TestComponent().Render()}</div>;
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toContain('<div id="fy-');
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toContain('<div id="fy-');
   });
 
   it('should return the neutralized outer html of a parsed jsx node with nested string mimicking a component', async () => {
     const jsxToParse = <div>{'<div id="fy-"></div><img src="fake" onerror="alert(1)">'}</div>;
     const expectedOuterHtml = '<div>&lt;div id="fy-"&gt;&lt;/div&gt;&lt;img src="fake" onerror="alert(1)"&gt;</div>';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toContain(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toContain(expectedOuterHtml);
   });
 
-  it('should set attributes defined in jsx', () => {
+  it('should set attributes defined in jsx', async () => {
     const jsxToParse = <div class="test"><p>test</p></div>;
     const expectedOuterHtml = '<div class="test"><p>test</p></div>';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
   });
 
-  it('should return jsx when a child is undefined', () => {
+  it('should return jsx when a child is undefined', async () => {
     const undefinedVariable = undefined;
     const jsxToParse = <div>{undefinedVariable}</div>;
     const expectedOuterHtml = '<div></div>';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
   });
 
   it('should add event listeners to bound events', async () => {
     let testVariable = 0;
     const jsxToParse = <button id="test-id" onclick={() => testVariable = 1}></button>;
-    const renderedHtml = JsxRenderer.RenderJsx(jsxToParse);
+    const renderedHtml = await JsxRenderer.RenderJsx(jsxToParse);
     document.body.innerHTML = renderedHtml;
 
     const eventProperlyBound = await TestHelpers.TimeLapsedCondition(() => {
@@ -66,46 +66,46 @@ describe('JsxRenderer', () => {
     document.body.innerHTML = Strings.Empty;
     const jsxToParse = <button id="test-id" onclick={() => null}></button>;
 
-    JsxRenderer.RenderJsx(jsxToParse);
+    await JsxRenderer.RenderJsx(jsxToParse);
 
     await TestHelpers.TimeLapsedCondition(() => {
       return expect(document.getElementById('test-id')).toBeNull();
     });
   });
 
-  it('should add guid ids for bound events if none are given', () => {
+  it('should add guid ids for bound events if none are given', async () => {
     const jsxToParse = <button onclick={() => true}></button>;
-    const renderedHtml = JsxRenderer.RenderJsx(jsxToParse);
+    const renderedHtml = await JsxRenderer.RenderJsx(jsxToParse);
     expect(renderedHtml.startsWith('<button id=')).toBeTruthy();
   });
 
-  it('should parse jsx containing fragments', () => {
+  it('should parse jsx containing fragments', async () => {
     const jsxToParse = <><p>test</p><p><><span>test</span></></p></>;
     const expectedOuterHtml = '<p>test</p><p><span>test</span></p>';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
   });
 
-  it('should parse jsx and render the text false', () => {
+  it('should parse jsx and render the text false', async () => {
     const jsxToParse = <>false</>;
     const expectedOuterHtml = 'false';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
   });
 
-  it('should parse jsx and NOT render the boolean value false', () => {
+  it('should parse jsx and NOT render the boolean value false', async () => {
     const jsxToParse = <>{false}</>;
     const expectedOuterHtml = '';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
   });
 
-  it('should parse jsx and render the right side of the && operator when the predicate is true', () => {
+  it('should parse jsx and render the right side of the && operator when the predicate is true', async () => {
     const jsxToParse = <>{true && 'test'}</>;
     const expectedOuterHtml = 'test';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
   });
 
-  it('should parse jsx and NOT render "false" when the && operator is used with a falsy predicate', () => {
+  it('should parse jsx and NOT render "false" when the && operator is used with a falsy predicate', async () => {
     const jsxToParse = <>{false && 'test'}</>;
     const expectedOuterHtml = '';
-    expect(JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
+    expect(await JsxRenderer.RenderJsx(jsxToParse)).toEqual(expectedOuterHtml);
   });
 });
